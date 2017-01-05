bef9159be5bfd51f11debdd20084adee48b6b9a9
Mark Birbeck <mark.birbeck@gmail.com>
2017-01-05 17:45:59 +0000

    remove all charting related pieces. they are now at: http://github.com/markbirbeck/fotochrom-web-components

--- index.js
+++ index.js
@@ -2,13 +2,5 @@ module.exports = {
   backplaneControl: require('./components/backplane-control'),
   backplaneOutput: require('./components/backplane-output'),
   widgetNumber: require('./components/widget-number'),
-  chartBar: require('./components/chart-bar'),
-  chartCohort: require('./components/chart-cohort'),
-  chartControl: require('./components/chart-control'),
-  chartGeo: require('./components/chart-geo'),
-  chartPie: require('./components/chart-pie'),
-  chartScatter: require('./components/chart-scatter'),
-  chartTimeSeries: require('./components/chart-time-series'),
-  plotTimeSeries: require('./components/plot-time-series'),
   xformsOutput: require('./components/xforms-output'),
 };
